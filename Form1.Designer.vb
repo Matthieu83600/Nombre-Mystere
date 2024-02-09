4d36a3a4162a6969de06092d1ba96e8f3118bbc9
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2024-02-09 17:31:17 +0100

    Création interface

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,10 +22,73 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 14F, FontStyle.Bold)
+        Label1.Location = New Point(73, 9)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(183, 25)
+        Label1.TabIndex = 0
+        Label1.Text = "Le nombre mystère"
+        Label1.TextAlign = ContentAlignment.TopCenter
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(25, 51)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(280, 15)
+        Label2.TabIndex = 1
+        Label2.Text = "Ce mini-jeu consiste à retrouver le nombre mystère."
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(28, 75)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(274, 15)
+        Label3.TabIndex = 2
+        Label3.Text = "Indice : le nombre mystère se trouve entre 1 et 100."
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = SystemColors.ButtonHighlight
+        Button1.FlatAppearance.BorderColor = Color.Black
+        Button1.FlatAppearance.BorderSize = 15
+        Button1.Font = New Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button1.Location = New Point(93, 118)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(131, 52)
+        Button1.TabIndex = 3
+        Button1.Text = "JOUER"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(334, 191)
+        Controls.Add(Button1)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Jeu : Le nombre mystère."
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button1 As Button
 
 End Class
